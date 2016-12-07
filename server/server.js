1642e1a0ddfe779832524464cd0608912568ea72
Bosco Li <boscoli@ca.ibm.com>
2016-12-07 15:01:11 -0500

    Relocate UI files

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.start = function() {
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
-  app.use(loopback.static(path.join(__dirname, '..')));
+  app.use(loopback.static(path.join(__dirname, '..', 'ui')));
   // start the server if `$ node server.js`
   if (require.main === module) {
     app.start();
